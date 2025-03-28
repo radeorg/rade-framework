a47929d1c814e423150b7be517c0dfad198ab301
lait <lait.zhang@gmail.com>
2025-03-28 15:57:33 +0800

    fix

--- rade-core/src/main/java/org/dows/rade/config/MyBatisFlexConfiguration.java
+++ rade-core/src/main/java/org/dows/rade/config/MyBatisFlexConfiguration.java
@@ -1,14 +1,14 @@
-package org.dows.rade.config;
-
-import com.mybatisflex.core.FlexGlobalConfig;
-import com.mybatisflex.spring.boot.MyBatisFlexCustomizer;
-
-//@Configuration
-public class MyBatisFlexConfiguration implements MyBatisFlexCustomizer {
-
-    @Override
-    public void customize(FlexGlobalConfig globalConfig) {
-        // 我们可以在这里进行一些列的初始化配置
-    }
-
-}
+//package org.dows.rade.config;
+//
+//import com.mybatisflex.core.FlexGlobalConfig;
+//import com.mybatisflex.spring.boot.MyBatisFlexCustomizer;
+//
+////@Configuration
+//public class MyBatisFlexConfiguration implements MyBatisFlexCustomizer {
+//
+//    @Override
+//    public void customize(FlexGlobalConfig globalConfig) {
+//        // 我们可以在这里进行一些列的初始化配置
+//    }
+//
+//}
