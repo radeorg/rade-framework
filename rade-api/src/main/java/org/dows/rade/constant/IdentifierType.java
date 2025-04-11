5fd82b7548530699b0f67cb2ad5e47efdbadad5a
lait <lait.zhang@gmail.com>
2025-04-11 23:01:08 +0800

    feat(core): 新增账号类型和开放渠道枚举

---
+++ rade-api/src/main/java/org/dows/rade/constant/IdentifierType.java
@@ -0,0 +1,37 @@
+package org.dows.rade.constant;
+
+import lombok.Getter;
+
+public enum IdentifierType {
+
+    ACCOUNT(1, "accountHandler", "账号&用户名"),
+
+    PHONE(2, "phoneHandler", "手机号"),
+
+    EMAIL(3, "emailHandler", "邮箱"),
+
+    WEIXIN(4, OpenChannel.WEIXIN.name(), "微信"),
+
+    QQ(5, "qqHandler", "QQ"),
+
+    ALIPAY(6, "alipayHandler", "支付宝"),
+
+    DOUYIN(7, "douyinHandler", "抖音"),
+
+    WEIBO(8, "weiboHandler", "微博"),
+
+    GITHUB(9, "githubHandler", "GitHub");
+
+    @Getter
+    private final int type;
+    @Getter
+    private final String channel;
+    @Getter
+    private final String description;
+
+    IdentifierType(int type, String channel, String description) {
+        this.type = type;
+        this.channel = channel;
+        this.description = description;
+    }
+}
---
+++ rade-api/src/main/java/org/dows/rade/constant/OpenChannel.java
@@ -0,0 +1,17 @@
+package org.dows.rade.constant;
+
+public enum OpenChannel {
+
+    ACCOUNT,
+    WEIXIN,
+    QQ,
+    WEIBO,
+    DOUYIN,
+    GITHUB,
+    ALIPAY,
+    WECHAT,
+    TOUTIAO,
+    BAIDU,
+    TENCENT,
+    ;
+}
