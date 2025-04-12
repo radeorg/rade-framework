76bf69c79da0a774b9aa24418fa930fbafe91944
lait <lait.zhang@gmail.com>
2025-04-13 00:04:39 +0800

    feat: 添加白名单过滤功能

--- rade-core/src/main/java/org/dows/rade/web/filter/AppContextSetupFilter.java
+++ rade-core/src/main/java/org/dows/rade/web/filter/AppContextSetupFilter.java
@@ -2,32 +2,50 @@ package org.dows.rade.web.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.dows.rade.context.AppContext;
-import org.dows.rade.exception.RadeException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 import java.io.IOException;
 
+@Data
 @Slf4j
 @RequiredArgsConstructor
 @Order(Ordered.HIGHEST_PRECEDENCE) // 设置高优先级
 public class AppContextSetupFilter implements Filter {
+    private String[] whitelist;
 
+    /*@Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String excludedUrlsParam = filterConfig.getInitParameter("whitelist");
+        if (excludedUrlsParam != null) {
+            whitelist = excludedUrlsParam.split(",");
+        }
+    }*/
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         try {
             if (request instanceof HttpServletRequest httpServletRequest) {
+                String requestURI = httpServletRequest.getRequestURI();
                 // 从请求头或参数获取appId
                 String appId = httpServletRequest.getHeader("AppId");
-                if (appId == null || appId.isBlank()) {
-                    throw new RadeException("appId不能为空");
-                }
                 // 设置appId到ThreadLocal
                 AppContext.setAppId(appId);
+                if (whitelist != null) {
+                    for (String item : whitelist) {
+                        if (requestURI.startsWith(item)) {
+                            chain.doFilter(request, response);
+                            return;
+                        }
+                    }
+                }
+                /*if (appId == null || appId.isBlank()) {
+                    throw new RadeException("appId不能为空");
+                }*/
                 chain.doFilter(request, response);
             }
         } finally {
