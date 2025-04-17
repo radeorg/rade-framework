76a46c2240001c745703d004a87fa04a48ce1cae
lait <lait.zhang@gmail.com>
2025-04-17 13:00:40 +0800

    feat(core): 新增AacContext接口及AacUser接口 fix: 修复maven.yml中的commit消息处理

---
+++ rade-api/src/main/java/org/dows/rade/aac/AacUser.java
@@ -0,0 +1,46 @@
+package org.dows.rade.aac;
+
+import java.util.List;
+
+
+public interface AacUser {
+
+    // 账号ID
+    Long getAccountId();
+
+    // 用户ID
+    Long getUserId();
+
+    // 角色集ID
+    List<Long> getRoleIds();
+    //void setRoleIds(List<Long> roleIds);
+
+    // 账号类型
+    List<Integer> getAccountTypes();
+    //void setAccountTypes(List<Integer> accountTypes);
+
+    // 超级账号
+    boolean isSuperAccount();
+
+    //账号名
+    String getNickname();
+
+    //用户名
+    String getUsername();
+
+    // 头像
+    String getAvatar();
+
+    //手机
+    String getTelephone();
+    //void setPhone(String phone);
+
+    // 邮箱
+    String getEmail();
+    //void setEmail(String email);
+    // 手机号绑定
+    Integer getState();
+
+    // 获取账号标识类型[weixin, qq, weibo, email...]
+    Integer getIdentifierType();
+}
