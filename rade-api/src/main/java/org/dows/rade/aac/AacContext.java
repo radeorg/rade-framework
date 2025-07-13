05866e01a76e9d8f9373c9252fe927daf0893972
lait <lait.zhang@gmail.com>
2025-07-13 13:19:05 +0800

    refactor(rade-api): 移除 AacContext 类中未使用的代码
    - 删除了 AacContext 类中未使用的默认方法 getWhitelist()
    - 该方法的实现已被移除，消除了冗余代码

--- rade-api/src/main/java/org/dows/rade/aac/AacContext.java
+++ rade-api/src/main/java/org/dows/rade/aac/AacContext.java
@@ -7,10 +7,6 @@ public interface AacContext {
         throw new UnsupportedOperationException("not implement!");
     }
 
-    /*default String[] getWhitelist() {
-        throw new UnsupportedOperationException("not implement!");
-    }*/
-
     default String[] getWhitelist(String appId){
         throw new UnsupportedOperationException("not implement!");
     }
