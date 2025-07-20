eecc31930cd01cb125a9bb440779a10242a5600d
lait <lait.zhang@gmail.com>
2025-07-20 18:17:20 +0800

    fix(web): 修复修改 URI逻辑
    - 修改了 modifyURI 方法中的逻辑，以正确处理包含多层路径的 URI
    - 此修复确保了在设置 AppContext时能够正确解析 URI

--- rade-core/src/main/java/org/dows/rade/web/filter/AppContextSetupFilter.java
+++ rade-core/src/main/java/org/dows/rade/web/filter/AppContextSetupFilter.java
@@ -54,7 +54,7 @@ public class AppContextSetupFilter implements Filter {
 
     private String modifyURI(String originalURI) {
         String substring = originalURI.substring(1);
-        originalURI = substring.substring(0, substring.indexOf("/"));
+        originalURI = substring.substring(substring.indexOf("/"));
         return originalURI;
     }
 
@@ -89,9 +89,8 @@ public class AppContextSetupFilter implements Filter {
                                 return;
                             }
                         }
-                    } else {
-                        chain.doFilter(uriWrapperRequest, response);
                     }
+                    chain.doFilter(uriWrapperRequest, response);
                 } else {
                     // 从请求头或参数获取appId
                     appId = httpServletRequest.getHeader("AppId");
